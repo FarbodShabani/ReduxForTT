b6f3a45a2ba0a32f5fb57dc99806f8e3462d90f1
Farbod Shabani <shabani.farbod@yahoo.com>
2020-03-22 19:55:45 +0430

    we add root Reducer to our reducer index.js by using combineReducer

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 import {Provider} from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import promiseMiddleware from 'redux-promise';
 
-AppRegistry.registerComponent(appName, () => App);
+import Reducers from './src/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(
+  createStore,
+);
+
+const appRedux = () => (
+  <Provider store={createStoreWithMiddleware(Reducers)}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => appRedux);
