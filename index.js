83f60347df574222c1e3d8f3cdc95511198cc6ba
Farbod Shabani <shabani.farbod@yahoo.com>
2020-03-22 19:28:26 +0430

    make configuration by adding redux reduxPromise and react-redux

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
 
 AppRegistry.registerComponent(appName, () => App);
